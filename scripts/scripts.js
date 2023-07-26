046e8fc223baa8d6a44505d626f6f7ceae71e1a9
Ricardo Vargas <rfcvargas00@gmail.com>
2023-07-26 14:36:13 -0300

    Organização do projeto2

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,4 +1,4 @@
 window.addEventListener("scroll", function(){
     let header = document.querySelector('#header')
-    header.classList.toggle('rolagem',window.scrollY > 500)
+    header.classList.toggle('rolagem',window.scrollY > 250)
 })
